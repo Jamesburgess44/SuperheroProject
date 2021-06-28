bb67225630cdb014db4b8d63979d51dda2cf5453
jamesburgess44 <jamesburgess44@yahoo.com>
2021-06-28 15:01:39 -0600

    displaying context data in template

--- superhero_creator/superhero/views.py
+++ superhero_creator/superhero/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Superhero
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'superhero/index.html')
+    all_superheros = Superhero.objects.all()
+    context = {
+        'all_superheros': all_superheros
+    }
+    return render(request, 'superhero/index.html', context)
